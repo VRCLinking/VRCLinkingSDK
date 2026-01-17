3f8605d2520fa0a15db007b87e25806943b0e5a6
Miner28_3 <suchys.jakub@gmail.com>
2026-01-17 09:42:18 +0100

    Release Basic Toggle Module

--- Packages/com.vrclinking.vrclinking-sdk/Runtime/Modules/Toggles/VrcLinkingToggleModule.cs
+++ Packages/com.vrclinking.vrclinking-sdk/Runtime/Modules/Toggles/VrcLinkingToggleModule.cs
@@ -3,6 +3,7 @@ using UdonSharp;
 using UnityEngine;
 using UnityEngine.UI;
 using VRC.SDK3.Data;
+using VRC.SDKBase;
 
 namespace VRCLinking.Modules.Toggles
 {
@@ -10,15 +11,37 @@ namespace VRCLinking.Modules.Toggles
     public class VrcLinkingToggleModule : VrcLinkingModuleBase
     {
         public override string ModuleName => "VrcLinkingToggleModule";
-
-        public DataDictionary toggleData;
-        /*
-         * 
-         */
+        public string supporterRoleId;
+        public GameObject[] toggleOnObjects;
+        public GameObject[] toggleOffObjects;
 
         public override void OnDataLoaded()
         {
+            if (!downloader.TryGetGuildMembersByRoleId(supporterRoleId, out DataList members)) return;
             
+            if (members.Contains(Networking.LocalPlayer.displayName))
+            {
+                ToggleObjects();
+            }
+        }
+
+        void ToggleObjects()
+        {
+            foreach (var obj in toggleOnObjects)
+            {
+                if (obj != null)
+                {
+                    obj.SetActive(true);
+                }
+            }
+            
+            foreach (var obj in toggleOffObjects)
+            {
+                if (obj != null)
+                {
+                    obj.SetActive(false);
+                }
+            }
         }
     }
 }
